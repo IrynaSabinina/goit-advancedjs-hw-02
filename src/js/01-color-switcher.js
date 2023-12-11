e27a6090853e20c7645a670d7cf0770919396976
Iryna <100072177+IrynaSabinina@users.noreply.github.com>
2023-12-11 18:27:40 +0100

    Initial commit

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,28 @@
+import '../css/common.css';
+
+const bodyEl = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+console.log(startBtn);
+const stoptBtn = document.querySelector('button[data-stop]');
+let timerId = null;
+
+startBtn.addEventListener('click', changeBodyColor);
+stoptBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  stoptBtn.disabled = true;
+  startBtn.disabled = false;
+});
+
+function changeBodyColor() {
+  startBtn.disabled = true;
+  stoptBtn.disabled = false;
+  timerId = setInterval(() => {
+    let colornew = getRandomHexColor();
+    bodyEl.style.backgroundColor = colornew;
+  }, 1000);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+console.log(getRandomHexColor());
---
+++ src/js/02-timer.js
@@ -0,0 +1,83 @@
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import '../css/common.css';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const userDateEl = document.querySelector('input#datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
+const daysSpanEl = document.querySelector('span[data-days]');
+const hoursSpanEl = document.querySelector('span[data-hours]');
+const minutesSpanEl = document.querySelector('span[data-minutes]');
+const secondsSpanEl = document.querySelector('span[data-seconds]');
+startBtn.disabled = true;
+let userData;
+// console.log(userData);
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(userData);
+    userData = selectedDates[0];
+    console.log(userData);
+    if (userData > Date.now()) {
+      startBtn.disabled = false;
+      // userDateEl.disabled = true;
+    } else {
+      alert('Please choose a date in the future');
+      return;
+    }
+  },
+};
+
+flatpickr(userDateEl, options);
+
+console.log(userData);
+startBtn.addEventListener('click', startTimer);
+
+function startTimer() {
+  console.log(userData);
+
+  if (userData > Date.now()) {
+    setInterval(() => {
+      const startTime = convertMs(userData - Date.now());
+      daysSpanEl.textContent = startTime.days;
+      hoursSpanEl.textContent = startTime.hours;
+      minutesSpanEl.textContent = startTime.minutes;
+      secondsSpanEl.textContent = startTime.seconds;
+    }, 1000);
+    startBtn.disabled = true;
+    userDateEl.disabled = true;
+  } else {
+    clearInterval();
+    startBtn.disabled = true;
+  }
+}
+
+function pad(value) {
+  if (value > 0) {
+    return String(value).padStart(2, '0');
+  } else {
+    return String(00).padStart(2, '0');
+  }
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = pad(Math.floor(ms / day));
+  // Remaining hours
+  const hours = pad(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
